1cb99b15e7ddb4ff55d9fcd3ede7e901dd95b2a4
Prajwal Negi <166615651+prajwalnegi2301@users.noreply.github.com>
2024-07-16 23:21:57 +0530

    Backend

---
+++ routes/connectWithUs.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {  isAuthenticated, isUserAuthenticated } from '../middlewares/auth.js';
+
+import { createConnectWithUs, getAllConnects } from '../controllers/connectWithUs.controllers.js';
+
+
+const router = express.Router();
+
+
+router.post('/createConnectMessage', createConnectWithUs);
+router.get('/getConnectMessages',isAuthenticated,getAllConnects);
+
+
+export default router;
